daf282b301ee88bee3d2dbf1d04ed45085bd80f9
Stephanie Hernandez <stephernandez831@gmail.com>
2023-03-25 16:07:57 -0700

    Created SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import SideBar from "./components/SideBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <SideBar></SideBar>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+import "./SideBar.css";
+
+const SideBar = () => {
+  return (
+    <div className="SideBar">
+      <h2>Marvel DataDashboard</h2>
+    </div>
+  );
+};
+
+export default SideBar;

99b9922b83a81de65312579c8aae58028ea6639b
Stephanie Hernandez <stephernandez831@gmail.com>
2023-03-25 16:09:16 -0700

    Created Menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import "./Menu.css";
+
+const Menu = () => {
+  return (
+    <div className="Menu">
+      <a href="/">Dashboard</a>
+      <a href="/">Seach</a>
+      <a href="/">About</a>
+    </div>
+  );
+};
+
+export default Menu;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,11 @@
+import Menu from "./Menu";
 import "./SideBar.css";
 
 const SideBar = () => {
   return (
     <div className="SideBar">
       <h2>Marvel DataDashboard</h2>
+      <Menu></Menu>
     </div>
   );
 };
